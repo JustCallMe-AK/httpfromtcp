a2d04eb327deb70acd6e1dadd6b731c2cfc5ca69
Akosa Erinne <akosa.erinne@gmail.com>
2025-05-31 17:57:49 -0400

    test to read and write data from streams

--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	// Open the file for reading
+	messages, fileOpenError := os.Open("messages.txt")
+	if fileOpenError != nil {
+		fmt.Printf("failed to open file: %v\n", fileOpenError)
+		return
+	}
+	defer messages.Close()
+
+	// Create a buffer to hold 8 bytes as you read the file
+	buffer := make([]byte, 8)
+
+	// Read the file
+	for {
+		offset, fileReadError := messages.Read(buffer)
+		if fileReadError != nil {
+			if fileReadError == io.EOF {
+				break
+			}
+			fmt.Printf("Error reading file: %v\n", fileReadError)
+			return
+		}
+
+		if offset > 0 {
+			fmt.Printf("read: %s\n", buffer[:offset])
+		}
+	}
 }
